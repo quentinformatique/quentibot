559d544e5eecf91b84e2186d0a276aa8a63eb31c
quentinou-le-nul <80545291+quentinou-le-nul@users.noreply.github.com>
2021-08-30 11:08:32 +0200

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -3,19 +3,19 @@ const lyricsFinder = require("lyrics-finder");
 
 exports.run = async(client, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
+    if (!queue) return message.channel.send("Aucun son n'est actuellement joué").catch(console.error);
 
     let lyrics = null;
 
     try {
       lyrics = await lyricsFinder(queue.songs[0].title, "");
-      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title} :(`;
+      if (!lyrics) lyrics = `pas de paroles trouvée pour ${queue.songs[0].title} :(`;
     } catch (error) {
-      lyrics = `No lyrics found for ${queue.songs[0].title} :(`;
+      lyrics = `pas de paroles trouvée pour ${queue.songs[0].title} :(`;
     }
 
     let lyricsEmbed = new MessageEmbed()
-      .setTitle(`Lyrics For ${queue.songs[0].title}`)
+      .setTitle(`Paroles de ${queue.songs[0].title}`)
       .setDescription(lyrics)
       .setColor("BLACK")
       .setTimestamp();

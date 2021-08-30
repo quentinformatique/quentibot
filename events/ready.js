a40dc4e6621a46711dfcb572fb0179f82bc89a22
quentinou-le-nul <80545291+quentinou-le-nul@users.noreply.github.com>
2021-08-30 11:27:14 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,4 @@
 module.exports = (client) => {
     console.log('Login as' + client.user.tag)
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
-    console.log(`phantom ©`);
+ 
 }
